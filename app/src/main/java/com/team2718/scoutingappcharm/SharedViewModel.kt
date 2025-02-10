725791e4c05d2820ac99b86eb5537a0dae5fa17a
Noah Zemlin <noah.zemlin@gmail.com>
2025-02-10 10:16:00 -0600

    lil guy

--- app/src/main/java/com/team2718/scoutingappcharm/SharedViewModel.kt
+++ app/src/main/java/com/team2718/scoutingappcharm/SharedViewModel.kt
@@ -3,9 +3,8 @@ package com.team2718.scoutingappcharm
 import android.app.Application
 import android.content.Context
 import android.os.AsyncTask
+import android.util.Log
 import androidx.lifecycle.AndroidViewModel
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import androidx.room.Room
 import com.team2718.scoutingappcharm.data_entity.ScoutingReport
@@ -17,29 +16,30 @@ class SharedViewModel(application: Application) : AndroidViewModel(application) 
     private val prefs = application.getSharedPreferences("team2718-prefs", Context.MODE_PRIVATE)
 
     private val db = Room.databaseBuilder(application, AppDatabase::class.java, "team2718-db").fallbackToDestructiveMigration().allowMainThreadQueries().build()
-    private val scouting_reports = db.scoutingReportDao()
+    private val scoutingReports = db.scoutingReportDao()
 
-    // This is dangerous being public, but I'm sure it'll be fine
     var currentReport: ScoutingReport = ScoutingReport(0)
 
     fun updateDB() {
-        AsyncTask.execute { scouting_reports.insertReplace(currentReport) }
-        prefs.edit().putInt("current_report", currentReport.uid).apply()
-        println("Wrote " + currentReport.uid + " to prefs!")
+        viewModelScope.launch { scoutingReports.insertReplace(currentReport) }
+        Log.i("SharedViewModel","Wrote ${currentReport.uid} to the database.")
     }
 
     fun newReport() {
         currentReport = ScoutingReport(Random.nextInt())
+        Log.i("SharedViewModel","Created new report ${currentReport.uid}.")
     }
 
     init {
         viewModelScope.launch {
-            if (prefs.contains("current_report")) {
-                println("Loading report " + prefs.getInt("current_report", 0))
-                val loadedReport = scouting_reports.loadId(prefs.getInt("current_report", 0))
+            val prefsCurrentReportID = prefs.getInt("current_report", 0)
+            if (prefsCurrentReportID != 0) {
+                println("Fetching report ${prefsCurrentReportID}...")
+                val loadedReport = scoutingReports.loadId(prefsCurrentReportID)
+
                 if (loadedReport != null) {
                     currentReport = loadedReport
-                    println("Loaded report with team number " + loadedReport.teamNumber + "!")
+                    println("Loaded report ${currentReport.uid}!")
                 }
             }
         }
--- app/src/main/java/com/team2718/scoutingappcharm/data_entity/ScoutingReport.kt
+++ app/src/main/java/com/team2718/scoutingappcharm/data_entity/ScoutingReport.kt
@@ -8,14 +8,31 @@ import androidx.room.PrimaryKey
 @Entity(indices = [Index(value = ["team_number"])], tableName = "reports")
 data class ScoutingReport (
     @PrimaryKey var uid: Int,
+
+    // Report info
     @ColumnInfo(name = "team_number") var teamNumber: Int? = 0,
     @ColumnInfo(name = "match_number") var matchNumber: Int? = 0,
     @ColumnInfo(name = "scout_name") var scoutName: String? = "",
+
+    @ColumnInfo(name = "is_complete") var isComplete: Boolean? = false, // Did the user press "Complete Report"?
+    @ColumnInfo(name = "has_been_scanned") var hasBeenScanned: Boolean? = false, // Was the QR code for this match scanned?
+
+    // Robot Abilities
     @ColumnInfo(name = "can_coral_station_intake") var canCoralStationIntake: Boolean? = false,
     @ColumnInfo(name = "can_coral_floor_intake") var canCoralFloorIntake: Boolean? = false,
     @ColumnInfo(name = "can_algae_reef_intake") var canAlgaeReefIntake: Boolean? = false,
     @ColumnInfo(name = "can_algae_reef_remove") var canAlgaeReefRemove: Boolean? = false,
     @ColumnInfo(name = "can_algae_floor_intake") var canAlgaeFloorIntake: Boolean? = false,
+    @ColumnInfo(name = "notes") var notes: String? = "",
+
+    // Auto
+    @ColumnInfo(name = "auto_L1") var autoL1: Int? = 0,
+    @ColumnInfo(name = "auto_L2") var autoL2: Int? = 0,
+    @ColumnInfo(name = "auto_L3") var autoL3: Int? = 0,
+    @ColumnInfo(name = "auto_L4") var autoL4: Int? = 0,
+    @ColumnInfo(name = "did_leave") var didLeave: Boolean? = false,
+
+    // Teleop
     @ColumnInfo(name = "num_processed") var numProcessed: Int? = 0,
     @ColumnInfo(name = "num_net_from_robot") var numNetFromRobot: Int? = 0,
     @ColumnInfo(name = "num_net_from_human") var numNetFromHuman: Int? = 0,
@@ -24,14 +41,9 @@ data class ScoutingReport (
     @ColumnInfo(name = "teleop_L2") var teleopL2: Int? = 0,
     @ColumnInfo(name = "teleop_L3") var teleopL3: Int? = 0,
     @ColumnInfo(name = "teleop_L4") var teleopL4: Int? = 0,
+
+    // Endgame
     @ColumnInfo(name = "deep_hang") var deepHang: Boolean? = false,
     @ColumnInfo(name = "shallow_hang") var shallowHang: Boolean? = false,
-    @ColumnInfo(name = "no_hang") var noHang: Boolean? = false,
-    @ColumnInfo(name = "auto_L1") var autoL1: Int? = 0,
-    @ColumnInfo(name = "auto_L2") var autoL2: Int? = 0,
-    @ColumnInfo(name = "auto_L3") var autoL3: Int? = 0,
-    @ColumnInfo(name = "auto_L4") var autoL4: Int? = 0,
-    @ColumnInfo(name = "did_leave") var didLeave: Boolean? = false,
-    @ColumnInfo(name = "notes") var notes: String? = "",
-    @ColumnInfo(name = "has_been_scanned") var hasBeenScanned: Boolean? = false
+    @ColumnInfo(name = "no_hang") var noHang: Boolean? = false
 )
